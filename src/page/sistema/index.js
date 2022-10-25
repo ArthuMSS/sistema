807eb0c2228e9ae8c52335e02127e9f66b10b537
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-10-25 09:34:06 -0300

    mais algm alterações

--- src/page/sistema/index.js
+++ src/page/sistema/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.scss'
 import { useState, useEffect } from 'react';
 import { useNavigate} from 'react-router-dom';
+import Contar from '../../services/contarAluno.js';
 
 export default function SistemaEscola(){
     const[qtdalunos, setQtdAlunos] = useState(0);
@@ -27,6 +28,7 @@ export default function SistemaEscola(){
 
                 <h2>Quantidade de alunos:</h2>
                 <input type="number"  placeholder='Digite a quantidade...'/>
+                <button>Calcular</button>
 
             </section>
 
---
+++ src/services/contarAluno.js
@@ -0,0 +1,7 @@
+
+export default function Contar(qtd){
+    let items = [];
+
+    for( i = 1; i <= qtd; i++){
+    }
+}
