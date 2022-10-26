44b28426442ba8016ab4303bc1616a4ad8de4d92
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-10-26 09:23:15 -0300

    pequena alt no sistema

--- src/page/sistema/index.js
+++ src/page/sistema/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate} from 'react-router-dom';
 import { contarArray, Maior, Media, Menor } from '../../services/funcoes';
 
